979edf3f5d8e232788875906bde637465d59e44c
masanov3167 <masanov3167@gmail.com>
2023-01-30 17:28:31 +0500

    first commit

--- src/page/AboutUsPage.tsx
+++ src/page/AboutUsPage.tsx
@@ -1,14 +1,11 @@
 import { Stack, Typography } from "@mui/material";
 import React, { ReactElement } from "react";
-
+import AboutHero from "../components/about/hero";
 const AboutUsPage = () : ReactElement =>{
     return <Stack>
-        <Typography variant="h2">H2</Typography>
-        <Typography variant="h3">H3</Typography>
-        <Typography variant="heroText1">HeroText1</Typography>
-        <Typography variant="heroText2">HeroText2</Typography>
-        <Typography variant="product">Product</Typography>
-        <Typography variant="buttonText">buttonText</Typography>
+        <Typography sx={{width:"100%", padding:"10px 150px"}} variant="paragraph">
+           <AboutHero />
+        </Typography>
     </Stack>
 }
 export default AboutUsPage
