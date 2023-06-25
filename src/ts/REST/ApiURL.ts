6232f0f7162374fc4fb831ed04549523e443eb17
Masanov Jumabek <masanov3167@gmail.com>
2023-06-25 09:46:30 +0500

    change back url

--- src/ts/REST/ApiURL.ts
+++ src/ts/REST/ApiURL.ts
@@ -1,9 +1,14 @@
 export function getApiURL(): string {
-  if (process.env.BUILD_TYPE === "uat") return "http://13.53.127.78:1337/api";
-  if (process.env.BUILD_TYPE === "dev") return "http://13.53.127.78:1337/api";
-  if (process.env.BUILD_TYPE === "uat") return "http://13.53.127.78:1337/api";
-  if (process.env.BUILD_TYPE === "dev") return "http://13.53.127.78:1337/api";
+  if (process.env.BUILD_TYPE === "uat")
+    return `${process.env.REACT_APP_BACKEND_URL}/api`;
+  if (process.env.BUILD_TYPE === "dev")
+    return `${process.env.REACT_APP_BACKEND_URL}/api`;
+  if (process.env.BUILD_TYPE === "uat")
+    return `${process.env.REACT_APP_BACKEND_URL}/api`;
+  if (process.env.BUILD_TYPE === "dev")
+    return `${process.env.REACT_APP_BACKEND_URL}/api`;
   if (process.env.BUILD_TYPE === "local") return "http://localhost:1337/api";
-  if (process.env.BUILD_TYPE === "prod") return "http://13.53.127.78:1337/api";
-  return "http://13.53.127.78:1337/api";
+  if (process.env.BUILD_TYPE === "prod")
+    return `${process.env.REACT_APP_BACKEND_URL}/api`;
+  return `${process.env.REACT_APP_BACKEND_URL}/api`;
 }
